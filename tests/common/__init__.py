edc6beaffbce67de801918afbf8fec1f08bc122f
Zeitsperre <trevor_smith@live.com>
2019-09-27 10:43:51 -0400

    Added some test data

---
+++ tests/common/__init__.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+data_path = Path.cwd()
+
+test_data = dict()
+test_data["current"] = data_path / "cmip5" / "tmax.cur.nc"
+test_data["future"] = data_path / "cmip5" / "tmax.fut.nc"
+test_data["observed"] = data_path / "cmip5" / "tmax.obs.nc"
--- tests/test_miranda.py
+++ tests/test_miranda.py
@@ -1,15 +1,18 @@
 """
 Tests for `miranda` module.
 """
+from pathlib import Path
+
 import miranda
+from .common import test_data
 
 
-class TestMirandaVersion(object):
+class TestMirandaVersion:
     def test_version(self):
         assert isinstance(miranda.__version__, str)
 
 
-class TestMiranda(object):
+class TestMiranda:
     @classmethod
     def setup_class(cls):
         pass
@@ -20,3 +23,15 @@ class TestMiranda(object):
     @classmethod
     def teardown_class(cls):
         pass
+
+
+class TestMakeClasses:
+    def test_database(self):
+        common = Path(__file__).parent
+        db = miranda.DataBase(common)
+        # db_keys = db.keys()
+
+        # TODO: Investigate why this doesn't work
+        # assert {"_files", "_is_server", "_source", "_destination"}.issubset(db_keys)
+        assert len(db.__dict__["_files"]) == 3
+        assert len(db.__dict__["_common_path"]) == 1
