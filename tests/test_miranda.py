de0a513b8cfa14743c0066e8c78541a1400b5cea
Zeitsperre <trevor_smith@live.com>
2019-10-03 16:31:54 -0400

    Fixed dictionary behaviour

--- tests/test_miranda.py
+++ tests/test_miranda.py
@@ -25,13 +25,37 @@ class TestMiranda:
         pass
 
 
-class TestMakeClasses:
-    def test_database(self):
+class TestDatabase:
+    def test_create_database(self):
         common = Path(__file__).parent
         db = miranda.DataBase(common)
-        # db_keys = db.keys()
 
-        # TODO: Investigate why this doesn't work
-        # assert {"_files", "_is_server", "_source", "_destination"}.issubset(db_keys)
-        assert len(db.__dict__["_files"]) == 3
-        assert len(db.__dict__["_common_path"]) == 1
+        assert len(db) == 3
+        assert str(db.__dict__["_common_path"]).endswith("tests/common/cmip5")
+
+    def test_dict_funcs(self):
+        common = Path(__file__).parent
+        db = miranda.DataBase(common)
+
+        true_keys = set(db.__dict__.keys())
+        assert {"_files", "_is_server", "_source", "_destination"}.issubset(true_keys)
+
+        keys = set(db.keys())
+        assert {
+            "project_name",
+            "recursive",
+            "successful_transfers",
+            "file_suffixes",
+        }.issubset(keys)
+        assert not {"_files", "_is_server", "_source", "_destination"}.issubset(keys)
+
+    def test_url_validator(self):
+        common = Path(__file__).parent
+        db = miranda.DataBase(common)
+
+        url = "https://www.google.ca"
+        short_url = "http://bit.ly/1a2b3c4d5e"
+        not_url = "htttp://not-a-url.biz"
+        assert db._url_validate(url)
+        assert db._url_validate(short_url)
+        assert not db._url_validate(not_url)
