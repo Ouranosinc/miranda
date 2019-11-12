31f8fdbcd93a5f62911693e424ab64d4a7f02e2f
Zeitsperre <trevor_smith@live.com>
2019-11-12 11:03:40 -0500

    Remove PyPy3 metadata

--- setup.py
+++ setup.py
@@ -64,6 +64,5 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: Implementation :: PyPy",
     ],
 )
