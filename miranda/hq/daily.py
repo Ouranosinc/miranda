moved units up one level. fixed bugs introduced with new API

miranda/hq/daily.py
@@ -9,51 +9,9 @@ from typing import Union
 
 import numpy as np
 import pandas as pd
-import pint
 import xarray as xr
+from miranda.units import u, units2pint
 
-
-def units():
-    """Return a unit environment."""
-    u = pint.UnitRegistry(autoconvert_offset_to_baseunit=True)
-    null = pint.Context("none")
-    u.add_context(null)
-
-    u.define("fraction = []")
-    u.define("percent = 1e-2 fraction = pct")
-    u.define("degC = kelvin; offset: 273.15 = celsius = C")
-    u.define("d = day")
-    u.define("h = hour")  # Not the Planck constant...
-    u.define("[speed] = [length] / [time]")
-
-    u.define("[precipitation] = [mass] / [length] ** 2 / [time]")
-    u.define("mmday = 1000 kg / meter ** 2 / day")
-
-    u.define("[discharge] = [length] ** 3 / [time]")
-    u.define("cms = meter ** 3 / second")
-
-    hydro = pint.Context("hydro")
-    hydro.add_transformation(
-        "[mass] / [length]**2",
-        "[length]",
-        lambda ureg, x: x / (1000 * ureg.kg / ureg.m ** 3),
-    )
-    hydro.add_transformation(
-        "[mass] / [length]**2 / [time]",
-        "[length] / [time]",
-        lambda ureg, x: x / (1000 * ureg.kg / ureg.m ** 3),
-    )
-    hydro.add_transformation(
-        "[length] / [time]",
-        "[mass] / [length]**2 / [time]",
-        lambda ureg, x: x * (1000 * ureg.kg / ureg.m ** 3),
-    )
-    u.add_context(hydro)
-    u.enable_contexts(hydro)
-    return u
-
-
-unites = units()
 
 # CMOR-like attributes
 cmor = json.load(open(Path(__file__).parent / "cf_attrs.json"))["variable_entry"]
@@ -176,12 +134,9 @@ def extract_daily(path) -> Tuple[dict, pd.DataFrame]:
 
 
 def to_cf(
-    meta: dict, data: pd.DataFrame, cf_table: Optional[dict] = None
+    meta: dict, data: pd.DataFrame, cf_table: Optional[dict] = {}
 ) -> xr.DataArray:
     """Return CF-compliant metadata."""
-
-    if dict is None:
-        cf_table = cmor
 
     # Convert meta values
     m = dict()
@@ -190,7 +145,7 @@ def to_cf(
 
     # Get default variable attributes
     name = guess_variable(m, cf_table)
-    attrs = cf_table[name]
+    attrs = cf_table.get(name)
 
     # Add custom HQ attributes
     for key, val in cf_attrs_names.items():
@@ -201,7 +156,7 @@ def to_cf(
 
     # Convert units
     if attrs["units"] != m["unité"]:
-        x = unites.convert(x, m["unité"], units2pint(attrs["units"]))
+        x = u.convert(x, m["unité"], units2pint(attrs["units"]))
 
     coords = {k: attrs.pop(k, np.nan) for k in ["lon", "lat", "elevation", "site"]}
     coords["time"] = data.index.values
@@ -211,44 +166,7 @@ def to_cf(
     return cf_corrected
 
 
-def open_csv(path: Union[str, Path], cf_table: Optional[dict] = None) -> xr.DataArray:
+def open_csv(path: Union[str, Path], cf_table: Optional[dict] = cmor) -> xr.DataArray:
     """Extract daily HQ meteo data and convert to xr.DataArray with CF-Convention attributes."""
     meta, data = extract_daily(path)
     return to_cf(meta, data, cf_table)
-
-
-def units2pint(value: str) -> pint.Quantity:
-    """Return the pint Unit for the DataArray units.
-
-    Parameters
-    ----------
-    value : str
-      Unit expression.
-
-    Returns
-    -------
-    pint.Quantity
-      Pint compatible units.
-
-    """
-
-    def _transform(s):
-        """Convert a CF-unit string to a pint expression."""
-        return re.subn(r"\^?(-?\d)", r"**\g<1>", s)[0]
-
-    value = value.replace("%", "pct")
-    try:  # Pint compatible
-        return unites.parse_expression(value).units
-    except (
-        pint.UndefinedUnitError,
-        pint.DimensionalityError,
-    ):  # Convert from CF-units to pint-compatible
-        return unites.parse_expression(_transform(value)).units
-
-
-# if __name__ == "__main__":
-#     fns = list(Path("/home/david/projects/ForestFires/data/HQ/LG_Tmin").glob("*.csv"))[
-#         :4
-#     ]
-#     das = [open_csv(fn) for fn in fns]
-#     da = xr.concat(das, dim="site")