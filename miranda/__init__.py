2026cb8f751d3ec314ea201e7d927af20e5cada8
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2021-03-23 18:03:11 -0400

    remove subset

--- miranda/__init__.py
+++ miranda/__init__.py
@@ -3,7 +3,7 @@ __email__ = "smith.trevorj@ouranos.ca"
 __version__ = "0.2.0-beta"
 
 
-from miranda import archive, gis, ops, remove, scripting, subset, utils
+from miranda import archive, gis, ops, remove, scripting, utils
 
 from .archiver import archive_database
 from .connect import Connection
