0c690a1e6602be38526f83bf1a21c0586158f829
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2021-05-07 16:48:26 -0400

    Add GitHub-specific assertions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -102,4 +102,7 @@ class TestReadPrivileges:
     def test_forbidden_folder_lax(self):
         root_folder = Path(Path.cwd().root).joinpath("root")
         allowed = utils.read_privileges(root_folder, strict=False)
-        assert allowed
+        if os.getenv("GITHUB_JOB"):
+            assert not allowed
+        else:
+            assert allowed
