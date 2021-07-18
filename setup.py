8a870cbdd0df1977826f0e1ee714cc961d75422d
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2021-07-18 17:37:15 -0400

    Fix circular import, update python versions

--- setup.py
+++ setup.py
@@ -61,9 +61,8 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
