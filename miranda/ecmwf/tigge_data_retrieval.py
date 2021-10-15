b82a45415a1b09e08871fa0a0273097cb09c4f49
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2021-10-15 13:32:20 -0400

    adjustment for tasmax and tasmin timesteps

--- miranda/ecmwf/tigge_data_retrieval.py
+++ miranda/ecmwf/tigge_data_retrieval.py
@@ -62,6 +62,17 @@ def collect_tigge(
         f"{variable_name}_{member}_{'-'.join(time.split('/'))}_tigge_reanalysis_6h_"
         f"{date.split('/')[0]}_{date.split('/')[-1]}.grib2"
     )
+
+    steps = (
+        "0/6/12/18/24/30/36/42/48/54/60/66/72/78/84/90/96/102/108/114/120/126/132/138/144/150/156/162/168/"
+        "174/180/186/192/198/204/210/216/222/228/234/240/246/252/258/264/270/276/282/288/294/300/306/312/318/324/"
+        "330/336/342/348/354/360"
+    )
+
+    # Remove steps 0 for tasmax and tasmin
+    if variable_code in [121, 122]:
+        steps = steps[2:]
+
     server = ECMWFDataServer()
     server.retrieve(
         {
@@ -74,9 +85,7 @@ def collect_tigge(
             "number": numbers,
             "origin": member,
             "param": variable_code,
-            "step": "0/6/12/18/24/30/36/42/48/54/60/66/72/78/84/90/96/102/108/114/120/126/132/138/144/150/156/162/168/"
-            "174/180/186/192/198/204/210/216/222/228/234/240/246/252/258/264/270/276/282/288/294/300/306/312/318/324/"
-            "330/336/342/348/354/360",
+            "step": steps,
             "time": time,
             "type": "pf",
             "target": output_name,
