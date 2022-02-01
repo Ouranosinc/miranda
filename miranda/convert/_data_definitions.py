1b7e3cf294cd7161c562b97f3973a0a13e5cddc0
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2022-02-01 15:44:12 -0500

    revise data definitions

--- miranda/convert/_data_definitions.py
+++ miranda/convert/_data_definitions.py
@@ -8,11 +8,12 @@ from miranda.scripting import LOGGING_CONFIG
 logging.config.dictConfig(LOGGING_CONFIG)
 
 __all__ = [
-    "gather_cfsr",
+    "gather_agcfsr",
+    "gather_agmerra",
     "gather_era5",
     "gather_era5_land",
-    "gather_merra2",
     "gather_nrcan_gridded_obs",
+    "gather_sc_earth",
     "gather_wfdei_gem_capa",
 ]
 
@@ -26,6 +27,13 @@ ERA5_VARIABLES = [
     "u10",
     "v10",
 ]
+
+
+NASA_AG_VARIABLES = ["prate", "rhstmax", "srad", "tavg", "tmax", "tmin", "wndpsd"]
+
+WFDEI_GEM_CAPA_VARIABLES = ["huss", "pr", "ps", "rlds", "rsds", "sfcWind", "tas"]
+
+SC_EARTH_VARIABLES = ["prcp", "tdew", "tmean", "trange", "wind"]
 
 
 def gather_era5(path: Union[str, os.PathLike]) -> Dict[str, List[Path]]:
@@ -58,50 +66,24 @@ def gather_era5_land(path: Union[str, os.PathLike]) -> Dict[str, List[Path]]:
     return {"era5-land": infiles_era5l}
 
 
-def gather_merra2(path: Union[str, os.PathLike]) -> Dict[str, List[Path]]:
-    # MERRA2 source data
-    source_merra2 = Path(path)
-    logging.info("Gathering MERRA2 from %s" % source_merra2.as_posix())
-    infiles_merra2 = list(sorted(source_merra2.rglob("tas_*.nc")))
-    infiles_merra2.extend(list(sorted(source_merra2.rglob("prbc*.nc"))))
-    return dict(merra2=infiles_merra2)
+def gather_agmerra(path: Union[str, os.PathLike]) -> Dict[str, List[Path]]:
+    # agMERRA source data
+    source_agmerra = Path(path)
+    logging.info("Gathering agMERRA from %s" % source_agmerra.as_posix())
+    infiles_agmerra = list()
+    for v in NASA_AG_VARIABLES:
+        infiles_agmerra.extend(list(sorted(source_agmerra.rglob(f"AgMERRA_*_{v}.nc4"))))
+    return dict(cfsr=infiles_agmerra)
 
 
-def gather_merra2_land_sea_mask(path: Union[str, os.PathLike]) -> Dict[str, Path]:
-    try:
-        land_sea_mask = dict(
-            sftlf=next(Path(path).joinpath("sftlf").glob("sftlf*.nc")),
-        )
-    except StopIteration:
-        logging.error("No land_sea_mask found for MERRA2.")
-        raise FileNotFoundError()
-    return land_sea_mask
-
-
-def gather_cfsr(path: Union[str, os.PathLike]) -> Dict[str, List[Path]]:
-    # CFSR source data
-    source_cfsr = Path(path)
-    logging.info("Gathering CFSR from %s" % source_cfsr.as_posix())
-    infiles_cfsr = list(sorted(source_cfsr.joinpath("tas").glob("tas_*.nc")))
-    infiles_cfsr.extend(
-        list(sorted(source_cfsr.joinpath("tasmax").glob("tasmax_*.nc")))
-    )
-    infiles_cfsr.extend(
-        list(sorted(source_cfsr.joinpath("tasmin").glob("tasmin_*.nc")))
-    )
-    infiles_cfsr.extend(list(sorted(source_cfsr.rglob("pr*.nc"))))
-    return dict(cfsr=infiles_cfsr)
-
-
-def gather_cfsr_land_sea_mask(path: Union[str, os.PathLike]) -> Dict[str, Path]:
-    try:
-        land_sea_mask = dict(
-            sftlf=next(Path(path).joinpath("sftlf").glob("sftlf*.nc")),
-        )
-    except StopIteration:
-        logging.error("No land_sea_mask found for CFSR.")
-        raise FileNotFoundError()
-    return land_sea_mask
+def gather_agcfsr(path: Union[str, os.PathLike]) -> Dict[str, List[Path]]:
+    # agCFSR source data
+    source_agcfsr = Path(path)
+    logging.info("Gathering CFSR from %s" % source_agcfsr.as_posix())
+    infiles_agcfsr = list()
+    for v in NASA_AG_VARIABLES:
+        infiles_agcfsr.extend(list(sorted(source_agcfsr.rglob(f"AgCFSR_*_{v}.nc4"))))
+    return dict(cfsr=infiles_agcfsr)
 
 
 def gather_nrcan_gridded_obs(path: Union[str, os.PathLike]) -> Dict[str, List[Path]]:
@@ -120,6 +102,19 @@ def gather_wfdei_gem_capa(path: Union[str, os.PathLike]) -> Dict[str, List[Path]
     # WFDEI-GEM-CaPa source data
     source_wfdei = Path(path)
     logging.info("Gathering WFDEI-GEM_CaPa from %s" % source_wfdei.as_posix())
-    infiles_wfdei = list(sorted(source_wfdei.glob("tas_*.nc")))
-    infiles_wfdei.extend(list(sorted(source_wfdei.glob("pr_*.nc"))))
-    return {"wfdei-gem-dapa": infiles_wfdei}
+    infiles_wfdei = list()
+    for v in WFDEI_GEM_CAPA_VARIABLES:
+        infiles_wfdei.extend(list(sorted(source_wfdei.rglob(f"{v}_*.nc"))))
+    return {"wfdei-gem-capa": infiles_wfdei}
+
+
+def gather_sc_earth(path: Union[str, os.PathLike]) -> Dict[str, List[Path]]:
+    # SC_Earth source data
+    source_sc_earth = Path(path)
+    logging.info("Gathering SC-Earth from %s" % source_sc_earth.as_posix())
+    infiles_sc_earth = list()
+    for v in SC_EARTH_VARIABLES:
+        infiles_sc_earth.extend(
+            list(sorted(source_sc_earth.rglob(f"SC-Earth_{v}_*.nc")))
+        )
+    return {"wfdei-gem-capa": infiles_sc_earth}
