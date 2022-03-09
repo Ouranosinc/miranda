b14e0ffde7e3eb3e4fcad23c5c5d4bd4c3924943
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2022-03-09 14:42:34 -0500

    More refactoring

--- miranda/metadata/__init__.py
+++ miranda/metadata/__init__.py
@@ -1,3 +1,30 @@
-from ._cmip import *
 from ._institutions import *
 from ._models import *
+from .cmip import *
+from .cmip import CMIP6_ACTIVITIES
+from .cordex import *
+
+ACTIVITIES = list()
+ACTIVITIES.extend(CMIP6_ACTIVITIES)
+ACTIVITIES.extend(["ERA"])
+
+
+WCRP_FREQUENCIES = [
+    "subhrPt",
+    "1hr",
+    "1hrCM",
+    "1hrPt",
+    "3hr",
+    "3hrPt",
+    "6hr",
+    "6hrPt",
+    "day",
+    "sem",
+    "mon",
+    "monC",
+    "monPT",
+    "yr",
+    "yrPt",
+    "dec",
+    "fx",
+]
---
+++ miranda/metadata/cmip/__init__.py
@@ -0,0 +1,1 @@
+from ._cmip import *
