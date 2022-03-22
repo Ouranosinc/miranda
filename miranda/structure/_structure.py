8ed230712aae4d8ccbd495d40faad39ce505ebd5
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2022-03-22 16:40:58 -0400

    move files with mpi

--- miranda/structure/_structure.py
+++ miranda/structure/_structure.py
@@ -1,4 +1,5 @@
 import logging.config
+import multiprocessing
 import os
 import shutil
 import sys
@@ -6,7 +7,8 @@ from pathlib import Path
 from types import GeneratorType
 from typing import List, Mapping, Optional, Union
 
-from miranda.decode import Decoder, guess_project
+from miranda import Decoder
+from miranda.decode import guess_project
 from miranda.scripting import LOGGING_CONFIG
 from miranda.utils import filefolder_iterator
 from miranda.validators import GRIDDED_SCHEMA, SIMULATION_SCHEMA, STATION_OBS_SCHEMA
@@ -121,9 +123,30 @@ def structure_datasets(
     -------
     dict
     """
-    input_files = filefolder_iterator(input_files, filename_pattern)
+    def _structure_datasets(in_file, out_path, m):
+        method_mod = ""
+        if in_file.is_dir():
+            method_mod = "tree"
+        if m.lower() in ["move", "copy"]:
+            meth = "Moved" if m.lower() == "move" else "Copied"
+            output_file = out_path.joinpath(in_file.name)
+            try:
+                if not dry_run:
+                    if sys.version_info < (3, 9):
+                        getattr(shutil, f"{m}{method_mod}")(
+                            str(in_file), str(output_file)
+                        )
+                    else:
+                        getattr(shutil, f"{m}{method_mod}")(in_file, output_file)
+                logging.info(f"{meth} {in_file.name} to {output_file}.")
+            except FileExistsError:
+                logging.warning(
+                    f"{in_file.name} already exists at location. Continuing..."
+                )
 
+    input_files = filefolder_iterator(input_files, filename_pattern)
     if not project and guess:
+        # Examine the first file from a list or generator
         for f in input_files:
             project = guess_project(f)
             decoder = Decoder(project)
@@ -145,26 +168,10 @@ def structure_datasets(
         for new_paths in set(all_file_paths.values()):
             Path(new_paths).mkdir(exist_ok=True, parents=True)
 
-    for file, output_filepath in all_file_paths.items():
-        method_mod = ""
-        if file.is_dir():
-            method_mod = "tree"
-        if method.lower() in ["move", "copy"]:
-            meth = "Moved" if method.lower() == "move" else "Copied"
-
-            output_file = output_filepath.joinpath(file.name)
-            try:
-                if not dry_run:
-                    if sys.version_info < (3, 9):
-                        getattr(shutil, f"{method}{method_mod}")(
-                            str(file), str(output_file)
-                        )
-                    else:
-                        getattr(shutil, f"{method}{method_mod}")(file, output_file)
-                logging.info(f"{meth} {file.name} to {output_file}.")
-            except FileExistsError:
-                logging.warning(
-                    f"{file.name} already exists at location. Continuing..."
-                )
+    # multiprocessing copy
+    pool = multiprocessing.Pool()
+    pool.starmap(_structure_datasets, zip(all_file_paths.keys(), all_file_paths.values()))
+    pool.close()
+    pool.join()
 
     return all_file_paths
