efff6668e7ae0772ddd8aabf64742d56ca804f16
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2022-03-31 15:29:27 -0400

    fix template call

--- miranda/templates/aws_cordex_download.py
+++ miranda/templates/aws_cordex_download.py
@@ -1,4 +1,5 @@
 import logging
+from os import getenv
 from pathlib import Path
 
 import dask
@@ -11,12 +12,16 @@ logger = logging.getLogger(__file__)
 dask.config.set(**{"array.slicing.split_large_chunks": True})
 
 if __name__ == "__main__":
+
+    out_files = getenv("out")
+
+    target_folder = Path(out_files)
+
     search = dict(
         variable=["tasmax", "tasmin", "tas", "pr"],
         scenario=["rcp45", "rcp85", "hist"],
         grid="NAM-22i",
         bias_correction="raw",
     )
-    target_folder = Path("/tank/smith/cordex_ncar_interpolated")
 
     cordex_aws_download(target_folder, search=search)
