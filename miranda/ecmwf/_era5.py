16350664f96f4517b2b02b7be59680280d2f7f23
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2022-05-10 02:54:38 -0400

    two month lag

--- miranda/ecmwf/_era5.py
+++ miranda/ecmwf/_era5.py
@@ -150,7 +150,8 @@ def request_era5(
         yearmonth = list()
         for y in years:
             for m in months:
-                if datetime.date(y, int(m) - 1, 1) < datetime.date.today():
+                two_months_lag = datetime.date.today() - datetime.timedelta(60)
+                if datetime.date(y, int(m), 1) > two_months_lag:
                     yearmonth.append((y, m))
 
         product = request_code.split("-")[0]
