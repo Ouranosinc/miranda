e87881ba0591105b846b01dac9240ce2817f048a
Pascal Bourgault <bourgault.pascal@ouranos.ca>
2022-11-01 13:27:35 -0400

    import annotations from future

--- miranda/convert/ec.py
+++ miranda/convert/ec.py
@@ -1,4 +1,5 @@
 # Convert ECCC data
+from __future__ import annotations
 from pathlib import Path
 
 import numpy as np
--- miranda/convert/melcc.py
+++ miranda/convert/melcc.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import datetime as dt
 import logging
 import re
