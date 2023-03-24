5d25d5eab7f947d34b6fee271f582172944de6fa
tlogan2000 <logan.travis@ouranos.ca>
2023-03-24 14:11:03 -0400

    concat_zarr for rdrs

--- templates/eccc_rdrs_processing.py
+++ templates/eccc_rdrs_processing.py
@@ -19,28 +19,28 @@ def main():
     )
     project = "rdrs-v21"
 
-    # convert_rdrs(
-    #     project=project,
-    #     input_folder=Path(home).joinpath("RDRS_v21", "caspar"),
-    #     output_folder=Path(home).joinpath("RDRS_v21", "tmp/ECCC/RDRS_v21/NAM"),
-    #     output_format="zarr",
-    #     working_folder=Path(home).joinpath("tmpout", "rdrs"),
-    #     **dask_kwargs,
-    # )
-    #
-    # rdrs_to_daily(
-    #     project=project,
-    #     input_folder=Path(home).joinpath(
-    #         "RDRS_v21", "tmp/ECCC/RDRS_v21/NAM/1hr"
-    #     ),
-    #     output_folder=Path(home).joinpath("RDRS_v21", "tmp/ECCC/RDRS_v21/NAM/day"),
-    #     working_folder=Path(home).joinpath("tmpout", "rdrs1"),
-    #     overwrite=False,
-    #     year_start=None,
-    #     year_end=None,
-    #     process_variables=None,
-    #     **dask_kwargs,
-    # )
+    convert_rdrs(
+        project=project,
+        input_folder=Path(home).joinpath("RDRS_v21", "caspar"),
+        output_folder=Path(home).joinpath("RDRS_v21", "tmp/ECCC/RDRS_v21/NAM"),
+        output_format="zarr",
+        working_folder=Path(home).joinpath("tmpout", "rdrs"),
+        **dask_kwargs,
+    )
+
+    rdrs_to_daily(
+        project=project,
+        input_folder=Path(home).joinpath(
+            "RDRS_v21", "tmp/ECCC/RDRS_v21/NAM/1hr"
+        ),
+        output_folder=Path(home).joinpath("RDRS_v21", "tmp/ECCC/RDRS_v21/NAM/day"),
+        working_folder=Path(home).joinpath("tmpout", "rdrs1"),
+        overwrite=False,
+        year_start=None,
+        year_end=None,
+        process_variables=None,
+        **dask_kwargs,
+    )
 
     for freq in ["day", "1hr"]:
         infolder = Path(home).joinpath("RDRS_v21", f"tmp/ECCC/RDRS_v21/NAM/{freq}")
