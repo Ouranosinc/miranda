8388c195d3af2dd3c2986ac1a866359062afb21d
juliettelavoie <juliette.lavoie@hotmail.ca>
2023-04-11 17:12:00 -0400

    comments

--- templates/nasa_nex-gddp-cmip6_processing.py
+++ templates/nasa_nex-gddp-cmip6_processing.py
@@ -5,9 +5,11 @@ from miranda import convert, io
 
 def main():
     path_nex_out = Path("/jarre/scenario/jlavoie/data/NEX-GDDP-CMIP6_downloaded/")
+    # put all nc that should be in 1 zarr together
     nex_files = convert.gather_nex(path_nex_out)
 
     for path, list_files in nex_files.items():
+        # open as dataset
         ds = convert.dataset_conversion(
             list_files,
             add_version_hashes=False,
@@ -15,10 +17,15 @@ def main():
             project="NEX-GDDP-CMIP6",
         )
         # the path is already ok, so just reuse it
-        end_path = path.split('downloaded/')[1]
+        end_path = path.split("downloaded/")[1]
 
+        # get right chunks
         calendar, size = io._rechunk.get_time_attrs(ds)
-        target_chunks = io._rechunk.translate_time_chunk({'time': '4 years', 'lat':50, 'lon':50}, calendar, size)
+        target_chunks = io._rechunk.translate_time_chunk(
+            {"time": "4 years", "lat": 50, "lon": 50}, calendar, size
+        )
+
+        # write to disk
         io.write_dataset(
             ds,
             project="NEX-GDDP-CMIP6",
@@ -26,7 +33,7 @@ def main():
             output_format="zarr",
             overwrite=True,
             compute=True,
-            chunks=target_chunks
+            chunks=target_chunks,
         )
 
 
