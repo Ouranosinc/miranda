4be12b15c5f4e4fc6f3145855521c02e97376c01
Pascal Bourgault <bourgault.pascal@ouranos.ca>
2023-05-12 17:33:19 -0400

    Add tests

---
+++ tests/test_structure.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+import pandas as pd
+import pytest  # noqa
+
+from miranda.structure import build_path_from_schema
+
+
+def get_testing_paths():
+    df = pd.read_csv(Path(__file__).parent / "structure_tests.csv")
+    for i, row in df.iterrows():
+        facets = dict(**row)
+        del facets["path"]
+        yield facets, row.path
+
+
+@pytest.mark.parametrize("facets,expected", get_testing_paths())
+def test_build_path_from_schema(facets, expected):
+    root = Path("/")
+    out = build_path_from_schema(
+        facets,
+        root,
+        top_folder="datasets"
+        if facets["processing_level"] in ["raw", "biasadjusted"]
+        else "derived",
+    ).relative_to(root)
+
+    assert str(out) == expected
