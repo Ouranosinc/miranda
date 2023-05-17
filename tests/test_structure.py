6ca5f0d0819624f72e404f3fade400818ce60563
Pascal Bourgault <bourgault.pascal@ouranos.ca>
2023-05-17 18:12:46 -0400

    remove derived datasets

--- tests/test_structure.py
+++ tests/test_structure.py
@@ -17,12 +17,7 @@ def get_testing_paths():
 @pytest.mark.parametrize("facets,expected", get_testing_paths())
 def test_build_path_from_schema(facets, expected):
     root = Path("/")
-    out = build_path_from_schema(
-        facets,
-        root,
-        top_folder="datasets"
-        if facets["processing_level"] in ["raw", "biasadjusted"]
-        else "derived",
-    ).relative_to(root)
+    out = build_path_from_schema(facets, root, top_folder="datasets")
 
-    assert str(out) == expected
+    assert out is not None, "Path not build, invalid facets"
+    assert str(out.relative_to(root)) == expected
