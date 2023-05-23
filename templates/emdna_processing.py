9a829493119341d934f804706eaa710e9b3cb0a2
juliettelavoie <juliette.lavoie@hotmail.ca>
2023-05-23 16:52:22 -0400

    add tasmax and tasmin

--- templates/emdna_processing.py
+++ templates/emdna_processing.py
@@ -11,6 +11,10 @@ def preprocess_dna(ds):
     ds = ds.rename_dims({"x": "lon", "y": "lat"})
     ds = ds.set_index(lat="latitude", lon="longitude", time="date")
     ds["time"] = [datetime.strptime(str(d), "%Y%m%d") for d in ds["time"].values]
+    ds["tasmax"] = ds["tmean"] + (
+        0.5 * ds["trange"]
+    )  # TODO: do this more miranda-like?
+    ds["tasmin"] = ds["tmean"] - (0.5 * ds["trange"])
     return ds
 
 
@@ -20,37 +24,38 @@ def main():
 
     files_by_member = convert.gather_emdna(path)
     for member, files in files_by_member.items():
-        ds = convert.dataset_conversion(
-            files, project="EMDNA", preprocess=preprocess_dna
-        )
-
-        # remove var we don't need
         if member == "OI":
-            ds = ds.drop_vars("pop")
+            ds = convert.dataset_conversion(
+                files, project="EMDNA", preprocess=preprocess_dna
+            )
 
-        # facets needed for building path
-        ds.attrs["member"] = member
-        facets = ds.attrs
-        for var in ds.data_vars:
-            del ds[var].attrs["description"]  # remove old description
-            facets["variable"] = var
+            # remove var we don't need
+            if member == "OI":
+                ds = ds.drop_vars("pop")
 
-            # get full path
-            new_path = structure.build_path_from_schema(facets, out_path)
-            # add version by hand for now
-            new_path = Path(str(new_path).replace("EMDNA", "EMDNA_v10"))
+            # facets needed for building path
+            ds.attrs["member"] = member
+            facets = ds.attrs
+            for var in ds.data_vars:
+                ds[var].attrs.pop("description", None)  # remove old description
+                facets["variable"] = var
 
-            if not os.path.exists(new_path):
-                with ProgressBar():
-                    # write to disk
-                    io.write_dataset(
-                        ds[[var]],
-                        project="EMDNA",
-                        output_path=new_path,
-                        output_format="zarr",
-                        overwrite=True,
-                        compute=True,
-                    )
+                # get full path
+                new_path = structure.build_path_from_schema(facets, out_path)
+                # add version by hand for now
+                new_path = Path(str(new_path).replace("EMDNA", "EMDNA_v10"))
+
+                if not os.path.exists(new_path):
+                    with ProgressBar():
+                        # write to disk
+                        io.write_dataset(
+                            ds[[var]],
+                            project="EMDNA",
+                            output_path=new_path,
+                            output_format="zarr",
+                            overwrite=True,
+                            compute=True,
+                        )
 
 
 if __name__ == "__main__":
