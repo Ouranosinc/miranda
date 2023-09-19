b5755cd9af20bb3727da0c8b098702c688f0ec66
tlogan2000 <logan.travis@ouranos.ca>
2023-09-19 10:40:04 -0400

    wind processing

--- templates/eccc_rdrs_processing.py
+++ templates/eccc_rdrs_processing.py
@@ -21,17 +21,17 @@ def main():
     )
     project = "rdrs-v21"
 
-    # convert_rdrs(
-    #     project=project,
-    #     input_folder=Path(home).joinpath("RDRS_v21", "caspar"),
-    #     output_folder=Path(home).joinpath("RDRS_v21", "tmp/ECCC/RDRS_v21/NAM"),
-    #     output_format="zarr",
-    #     working_folder=Path(home).joinpath("tmpout", "rdrs"),
-    #     cfvariable_list=vars_to_process,
-    #     overwrite=False,
-    #     **dask_kwargs,
-    # )
-    #
+    convert_rdrs(
+        project=project,
+        input_folder=Path(home).joinpath("RDRS_v21", "caspar"),
+        output_folder=Path(home).joinpath("RDRS_v21", "tmp/ECCC/RDRS_v21/NAM"),
+        output_format="zarr",
+        working_folder=Path(home).joinpath("tmpout", "rdrs"),
+        cfvariable_list=vars_to_process,
+        overwrite=False,
+        **dask_kwargs,
+    )
+
     # rdrs_to_daily(
     #     project=project,
     #     input_folder=Path(home).joinpath("RDRS_v21", "tmp/ECCC/RDRS_v21/NAM/1hr"),
@@ -43,22 +43,22 @@ def main():
     #     process_variables=vars_to_process,  # huss
     #     **dask_kwargs,
     # )
-    #
-    for freq in ["1hr"]:
-        infolder = Path(home).joinpath("RDRS_v21", f"tmp/ECCC/RDRS_v21/NAM/{freq}")
-        for variable in [
-            v for v in infolder.glob("*") if v.is_dir() and v.name in vars_to_process
-        ]:
-            concat_rechunk_zarr(
-                project=project,
-                freq=freq,
-                input_folder=variable,
-                output_folder=Path(home).joinpath(
-                    "RDRS_v21", f"converted/ECCC/RDRS_v21/NAM/{freq}/{variable.name}"
-                ),
-                overwrite=True,
-                **dask_kwargs,
-            )
+    # #
+    # for freq in ["day", "1hr"]:
+    #     infolder = Path(home).joinpath("RDRS_v21", f"tmp/ECCC/RDRS_v21/NAM/{freq}")
+    #     for variable in [
+    #         v for v in infolder.glob("*") if v.is_dir() and v.name in vars_to_process
+    #     ]:
+    #         concat_rechunk_zarr(
+    #             project=project,
+    #             freq=freq,
+    #             input_folder=variable,
+    #             output_folder=Path(home).joinpath(
+    #                 "RDRS_v21", f"converted/ECCC/RDRS_v21/NAM/{freq}/{variable.name}"
+    #             ),
+    #             overwrite=False,
+    #             **dask_kwargs,
+    #         )
 
 
 if __name__ == "__main__":
