d20be40dc45aaada37726df9244eff25d9fc9679
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2024-03-19 11:20:56 -0400

    update attr treatments

--- miranda/treatments/__init__.py
+++ miranda/treatments/__init__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import datetime
 import logging.config
 
-import xarray as xr
+import xarray
 
 from miranda import __version__ as __miranda_version__
 from miranda.scripting import LOGGING_CONFIG
@@ -19,7 +19,7 @@ logging.config.dictConfig(LOGGING_CONFIG)
 VERSION = datetime.datetime.now().strftime("%Y.%m.%d")
 
 
-def metadata_conversion(d: xr.Dataset, p: str, m: dict) -> xr.Dataset:
+def metadata_conversion(d: xarray.Dataset, p: str, m: dict) -> xarray.Dataset:
     """Update xarray dataset and data_vars with project-specific metadata fields.
 
     Parameters
@@ -67,36 +67,35 @@ def metadata_conversion(d: xr.Dataset, p: str, m: dict) -> xr.Dataset:
         del m["Header"]["_frequency"]
 
     # Conditional handling of global attributes based on project name
-    for field in [f for f in header if f.startswith("_")]:
+    for field in [f for f in header.keys() if f.startswith("_")]:
         if isinstance(header[field], list):
             if p in header[field]:
-                attr_treatment = header[field][p]
+                attr_treatments = header[field][p]
             else:
                 logging.warning(
                     f"Attribute handling (`{field}`) not set for project `{p}`. Continuing..."
                 )
                 continue
         elif isinstance(header[field], dict):
-            attr_treatment = header[field]
+            attr_treatments = header[field]
         else:
             raise AttributeError(
                 f"Attribute treatment configuration for field `{field}` is not properly configured. Verify JSON."
             )
 
+        if field[1:] in d.attrs:
+            logging.warning(f"Overwriting `{field[1:]}` based on JSON configuration.")
         if field == "_map_attrs":
-            for attribute, mapping in attr_treatment.items():
+            for attribute, mapping in attr_treatments.items():
                 header[mapping] = d.attrs[attribute]
                 del d.attrs[attribute]
         elif field == "_remove_attrs":
-            for ff in attr_treatment:
+            for ff in attr_treatments:
                 del d.attrs[ff]
+        elif field.startswith("_") and p in attr_treatments:
+            header[field[1:]] = attr_treatments[p]
         else:
-            if field[1:] in d.attrs:
-                logging.warning(
-                    f"Overwriting `{field[1:]}` based on JSON configuration."
-                )
-            header[field[1:]] = attr_treatment
-
+            header[field[1:]] = attr_treatments
         del header[field]
 
     # Add global attributes
