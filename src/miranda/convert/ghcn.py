1edec70d8b5a9a824f0aeae9c4974465ff07c85a
tlogan2000 <logan.travis@ouranos.ca>
2025-03-04 11:38:21 -0500

    remove test code

--- src/miranda/convert/ghcn.py
+++ src/miranda/convert/ghcn.py
@@ -129,10 +129,6 @@ def create_ghcn_xarray(infolder: Path, varmeta: dict, statmeta: pd.DataFrame) ->
                         ds[vv] = ds[vv].where(ds[vv] != nn, "")
 
             data.append(ds)
-            for vv in ds.data_vars:
-                if "flag" in vv:
-                    print(vv)
-                    print(np.unique(ds[vv].values))
     if len(data) == 0:
         return None
     return xr.concat(data, dim="station")
