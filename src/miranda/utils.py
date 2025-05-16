baef3ad17d5246721c4eea120d5081d491b55030
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2025-05-16 15:46:14 -0400

    add cassini and testing helpers, refactoring

--- src/miranda/utils.py
+++ src/miranda/utils.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 import gzip
 import logging
 import os
-import re
 import sys
 import tarfile
 import tempfile
@@ -13,16 +12,13 @@ import warnings
 import zipfile
 from collections.abc import Iterable, Sequence
 from contextlib import contextmanager
-from io import StringIO
 from pathlib import Path
-from typing import TextIO
 
 __all__ = [
     "HiddenPrints",
     "chunk_iterables",
     "generic_extract_archive",
     "list_paths_with_elements",
-    "publish_release_notes",
     "single_item_list",
     "working_directory",
 ]
@@ -277,84 +273,6 @@ def list_paths_with_elements(
             for my_path, my_item in zip(next_base_paths, path_content):
                 paths_elements.append({"path": my_path, elements[0]: my_item})
     return paths_elements
-
-
-def publish_release_notes(
-    style: str = "md", file: os.PathLike | StringIO | TextIO | None = None
-) -> str | None:
-    """
-    Format release history in Markdown or ReStructuredText.
-
-    Parameters
-    ----------
-    style : {"rst", "md"}
-        Use ReStructuredText formatting or Markdown. Default: Markdown.
-    file : {os.PathLike, StringIO, TextIO}, optional
-        If provided, prints to the given file-like object.
-        Otherwise, returns a string.
-
-    Returns
-    -------
-    str, optional
-        The formatted string if a file object is not provided.
-
-    Notes
-    -----
-    This function is solely for development purposes.
-    """
-    history_file = Path(__file__).parent.parent.joinpath("HISTORY.rst")
-
-    if not history_file.exists():
-        raise FileNotFoundError("History file not found in miranda file tree.")
-
-    with Path(history_file).open("r") as hf:
-        history = hf.read()
-
-    if style == "rst":
-        hyperlink_replacements = {
-            r":issue:`([0-9]+)`": r"`GH/\1 <https://github.com/Ouranosinc/miranda/issues/\1>`_",
-            r":pull:`([0-9]+)`": r"`PR/\1 <https://github.com/Ouranosinc/miranda/pull/\>`_",
-            r":user:`([a-zA-Z0-9_.-]+)`": r"`@\1 <https://github.com/\1>`_",
-        }
-    elif style == "md":
-        hyperlink_replacements = {
-            r":issue:`([0-9]+)`": r"[GH/\1](https://github.com/Ouranosinc/miranda/issues/\1)",
-            r":pull:`([0-9]+)`": r"[PR/\1](https://github.com/Ouranosinc/miranda/pull/\1)",
-            r":user:`([a-zA-Z0-9_.-]+)`": r"[@\1](https://github.com/\1)",
-        }
-    else:
-        raise NotImplementedError()
-
-    for search, replacement in hyperlink_replacements.items():
-        history = re.sub(search, replacement, history)
-
-    if style == "md":
-        history = history.replace(".. :changelog:\n\n", "")
-        history = history.replace("=======\nHistory\n=======", "# History")
-
-        titles = {r"\n(.*?)\n([\-]{1,})": "-", r"\n(.*?)\n([\^]{1,})": "^"}
-        for title_expression, level in titles.items():
-            found = re.findall(title_expression, history)
-            for grouping in found:
-                fixed_grouping = (
-                    str(grouping[0]).replace("(", r"\(").replace(")", r"\)")
-                )
-                search = rf"({fixed_grouping})\n([\{level}]{'{' + str(len(grouping[1])) + '}'})"
-                replacement = f"{'##' if level == '-' else '###'} {grouping[0]}"
-                history = re.sub(search, replacement, history)
-
-        link_expressions = r"[\`]{1}([\w\s]+)\s<(.+)>`\_"
-        found = re.findall(link_expressions, history)
-        for grouping in found:
-            search = rf"`{grouping[0]} <.+>`\_"
-            replacement = f"[{str(grouping[0]).strip()}]({grouping[1]})"
-            history = re.sub(search, replacement, history)
-
-    if not file:
-        return history
-    if isinstance(file, (Path, os.PathLike)):
-        file = Path(file).open("w")
-    print(history, file=file)
 
 
 def read_privileges(location: str | Path, strict: bool = False) -> bool:
