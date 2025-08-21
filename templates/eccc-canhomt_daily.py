cf23125f1c4fdaa0cd4e1e98240344dc956875b6
tlogan2000 <logan.travis@ouranos.ca>
2025-08-21 09:51:57 -0400

    initial template

---
+++ templates/eccc-canhomt_daily.py
@@ -0,0 +1,76 @@
+import logging
+from pathlib import Path
+import shutil
+from dask.diagnostics import ProgressBar
+from miranda.convert.ghcn import convert_statdata_bychunks, download_canhomt, q_flag_dict
+import xarray as xr
+
+
+def main():
+
+    logging.basicConfig(level=logging.INFO)
+    working_folder = Path.home().joinpath("CanHomTV4/CanHomT_dlyV4")
+    
+
+    lon_bnds = [-80, -75]
+    lat_bnds = [44, 45]
+
+    nstations = 100
+
+    start_year = 1800
+    end_year = None
+
+    update_raw = False
+
+    # download station data
+    download_canhomt(
+        project="canhomt_dly",
+        working_folder=working_folder,
+        update_raw=update_raw,
+    )
+
+
+    # convert ghcn data by chunks of nstations
+    convert_statdata_bychunks(
+        project="canhomt_dly",
+        working_folder=working_folder,
+        lon_bnds=lon_bnds,
+        lat_bnds=lat_bnds,
+        start_year=start_year,
+        end_year=end_year,
+        nstations=nstations,
+        n_workers=6,
+    )
+
+    # combine zarrs
+    for var1 in [v for v in working_folder.joinpath("zarr").iterdir() if v.is_dir()]:
+        outfolder = working_folder.joinpath(f"final/{var1.name}")
+        outfolder.mkdir(parents=True, exist_ok=True)
+        if outfolder.exists():
+            shutil.rmtree(outfolder)
+        inzarrs = sorted(var1.glob("*.zarr"))
+        ds = xr.concat(
+            [xr.open_zarr(z, decode_timedelta=False) for z in inzarrs], dim="station"
+        ).sortby(["station", "time"])
+        outzarr = outfolder.joinpath(
+            f'{var1.name}_day_GovCan_CamHomT_v4_{ds.time[0].dt.strftime("%Y%m%d").values}-{ds.time[-1].dt.strftime("%Y%m%d").values}.zarr'
+        )
+        # remove encoding
+        for c in ds.coords:
+            ds[c].load()
+            ds[c].encoding = {}
+        for c in ds.data_vars:
+            ds[c].encoding = {}
+            if "flag" in c:
+                mask = ds[c].isin(list(q_flag_dict.keys()))
+                ds[c] = ds[c].where(mask, "")
+                ds[c] = ds[c].astype(str)
+        with ProgressBar():
+            ds.chunk(dict(station=250, time=365 * 4 + 1)).to_zarr(
+                outzarr.with_suffix(".tmp.zarr"), mode="w"
+            )
+        shutil.move(outzarr.with_suffix(".tmp.zarr"), outzarr)
+
+
+if __name__ == "__main__":
+    main()
