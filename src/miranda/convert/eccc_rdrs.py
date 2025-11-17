3232d167d5e4fd2f3069c579706556a162c38195
Asli Bese <abese@doris.ouranos.ca>
2025-11-17 14:14:34 -0500

    added entries for casr v3.2

--- src/miranda/convert/eccc_rdrs.py
+++ src/miranda/convert/eccc_rdrs.py
@@ -27,6 +27,7 @@ CONFIG_FOLDER = pathlib.Path(__file__).parent / "data"
 CONFIG_FILES = {
     "rdrs-v21": "eccc_rdrs_cf_attrs.json",
     "casr-v31": "eccc_casr_cf_attrs.json",
+    "casr-v32": "eccc_casr_cf_attrs.json",
     "ORRC-v10": "ouranos_orrc_cf_attrs.json",
     "ORRC-v11": "ouranos_orrc_cf_attrs.json",
 }
