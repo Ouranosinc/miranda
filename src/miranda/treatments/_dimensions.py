e9a57ff802dd7386bb2aac5946d0c6ee5b805f9d
tlogan2000 <logan.travis@ouranos.ca>
2025-12-10 10:47:25 -0500

    daily_snapshot correction functionality

--- src/miranda/treatments/_dimensions.py
+++ src/miranda/treatments/_dimensions.py
@@ -16,6 +16,7 @@ __all__ = [
     "ensure_correct_time_frequency",
     "find_project_variable_codes",
     "offset_time_dimension",
+    "get_daily_snapshot",
 ]
 
 
@@ -199,6 +200,41 @@ def ensure_correct_time_frequency(d: xr.Dataset, p: str, m: dict) -> xr.Dataset:
 
     return d
 
+def get_daily_snapshot(d: xr.Dataset, p: str, m: dict) -> xr.Dataset:
+    """
+    Get a single hour snapshot per day from a sub-daily dataset.
+
+    Parameters
+    ----------
+    ds : xarray.Dataset
+        The dataset.
+    snapvalue : int or bool
+
+    Returns
+    -------
+    xarray.Dataset
+        The dataset with the snapshot hour applied.
+    """
+    key = "_use_snapshot"
+    converted = []
+    for vv in d.data_vars:
+        if vv in m["variables"].keys():
+            #clip_values = _get_section_entry_key(m, "variables", vv, key, p)
+            snapvalue = _get_section_entry_key(m, "variables", vv, key, p)
+            if isinstance(snapvalue, int) and snapvalue is not True:
+                mask_hour = d.time.dt.hour == int(snapvalue)
+                d = d.sel(time=mask_hour)
+            elif snapvalue is True:
+                # if True, we assumme data is only available at a single hour each day : 
+                # ex. CaSR snow depth is padded with NaNs
+                d = d.dropna(dim="time", how="all")
+            else:
+                raise ValueError(f"Invalid _use_snapshot value: {snapvalue}.")
+            if xr.infer_freq(d.time) == "D": # "After applying snapshot, the time frequency must be daily."
+                return d
+            else:
+                msg = f"After applying snapshot, the time frequency is not daily. Found frequency: {xr.infer_freq(d.time)}."
+                raise ValueError(msg)
 
 def offset_time_dimension(d: xr.Dataset, p: str, m: dict) -> xr.Dataset:
     """Offset time dimension using listed frequency."""
