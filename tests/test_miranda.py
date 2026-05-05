002050fba14cf9c08b29a359c2c59642011d5685
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2026-05-05 16:01:05 -0400

    Bump version: 0.7.0-dev.0 → 0.7.0

--- tests/test_miranda.py
+++ tests/test_miranda.py
@@ -24,4 +24,4 @@ def test_package_metadata():
         contents = f.read()
         assert """Trevor James Smith""" in contents
         assert '__email__ = "smith.trevorj@ouranos.ca"' in contents
-        assert '__version__ = "0.7.0-dev.0"' in contents
+        assert '__version__ = "0.7.0"' in contents
