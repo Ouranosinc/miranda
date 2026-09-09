3088854b24ff5413d197be5ec696ae0dedc153e7
tlogan2000 <logan.travis@ouranos.ca>
2026-09-09 15:21:14 -0400

    method being rewritten

--- src/miranda/convert/_aggregation.py
+++ src/miranda/convert/_aggregation.py
@@ -188,7 +188,7 @@ def aggregate(ds: xr.Dataset, freq: str = "day") -> dict[str, xr.Dataset]:
                 aggregated[transformed] = ds_out
             else:
                 ds_out[transformed] = getattr(r, op)(dim="time", keep_attrs=True)
-            method = f"time: {op}{'imum' if op != 'mean' else ''} (interval: 1 {freq})"
+                method = f"time: {op}{'imum' if op != 'mean' else ''} (interval: 1 {freq})"
             ds_out[transformed].attrs["cell_methods"] = method
             aggregated[transformed] = ds_out
 
