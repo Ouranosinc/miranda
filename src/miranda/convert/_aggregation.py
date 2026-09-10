9e23855be9999e75346da6f0c99ace29015eb72b
Travis Logan <logan.travis@ouranos.ca>
2026-09-10 11:03:13 -0400

    Update src/miranda/convert/_aggregation.py
    Co-authored-by: Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
    Signed-off-by: Travis Logan <logan.travis@ouranos.ca>

--- src/miranda/convert/_aggregation.py
+++ src/miranda/convert/_aggregation.py
@@ -212,7 +212,7 @@ def _circular_mean(da: xr.DataArray, units: str, dim: str = "time", high: float 
         # convert back to degrees
         out = np.rad2deg(np.arctan2(sin_sum, cos_sum))
     # ensure the result is within the specified low, high range
-    if normalize is True:
+    if normalize:
         out = ((out - low) % (high - low)) + low
     out.attrs = da.attrs.copy()
 
